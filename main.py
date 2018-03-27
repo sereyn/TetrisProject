ffee4dc075393ee5c55a72a66dc9fe07440c9fdf
QuentinJanuel <facemashldv@laposte.net>
2018-03-27 13:15:28 +0200

    Fixing stuff

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from qtido import *
 
-<<<<<<< HEAD
 win = creer(800, 600)
-=======
 FPS = 60
->>>>>>> origin/Quentin
 
-#test
 while not est_fermee(win):
 	#Content incoming lol
 	attendre_pendant(win, 1000/FPS)
